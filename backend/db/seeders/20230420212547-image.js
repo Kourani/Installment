684af6081c923f2e59e40687d8ae64b5fefc2f8b
Kourani <76729542m@gmail.com>
2023-07-17 12:11:14 -0400

    something is wrong with the images on render

--- backend/db/seeders/20230420212547-image.js
+++ backend/db/seeders/20230420212547-image.js
@@ -175,7 +175,14 @@ module.exports = {
 
     },
     {
-      url:'https://images.pexels.com/photos/139396/lavender-flowers-blue-flowers-purple-139396.jpeg',
+      url:'https://images.pexels.com/photos/2387965/pexels-photo-2387965.jpeg',
+      preview:true,
+      imagableType:'Spot',
+      imagableId:8
+
+    },
+    {
+      url:'https://images.pexels.com/photos/2387965/pexels-photo-2387965.jpeg',
       preview:true,
       imagableType:'Spot',
       imagableId:8
