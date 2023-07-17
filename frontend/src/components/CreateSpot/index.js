129e049e5a14438f56d2ca33482ac006718daaa6
Kourani <76729542m@gmail.com>
2023-07-17 09:05:51 -0400

    fixed the errors object issue

--- frontend/src/components/CreateSpot/index.js
+++ frontend/src/components/CreateSpot/index.js
@@ -109,6 +109,7 @@ function CreateSpot(){
 
         if(!url || (!(url.endsWith('.png') || url.endsWith('.jpg') || url.endsWith('.jpeg') ) ) ) {
 
+                const errors = {}
                 if(!country) errors['country']='Country is required'
                 if(!address) errors['streetAddress']='Address is required'
                 if(!city) errors['city']='City is required'
@@ -128,9 +129,11 @@ function CreateSpot(){
                 if(image3 && !(image3.endsWith('.png') || image3.endsWith('.jpg') || image3.endsWith('.jpeg'))) errors['image3']='Image URL must end in .png .jpg or .jpeg'
                 if(image4 && !(image4.endsWith('.png') || image4.endsWith('.jpg') || image4.endsWith('.jpeg'))) errors['image4']='Image URL must end in .png .jpg or .jpeg'
 
+                setValidationErrors(errors)
+
         }
-        
-        if(url && (!(url.endsWith('.png') || url.endsWith('.jpg') || url.endsWith('.jpeg') ) )){
+
+        else if(url && (!(url.endsWith('.png') || url.endsWith('.jpg') || url.endsWith('.jpeg') ) )){
         try {
             const created = await dispatch(spotActions.createSpot(payload));
             console.log('TRY BLOCK....CREATED',created)
